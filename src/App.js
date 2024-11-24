7f80122de82839feadb0625723b1c4595cabb338
Sonali Kanawade <sonalikanawade111@gmail.com>
2024-11-24 15:19:22 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
-
+import CustomInpunt from './components/CustomInput';
 function App() {
+  const inputRef = useRef()
+  const handleClear = () => {
+    inputRef.current.clear()
+  }
+  const handleFocus = () => {
+    inputRef.current.focus()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useImperativeHandle to send child to parent data in react</h1>
+      <CustomInpunt ref={inputRef}/>
+      <button onClick={handleFocus}>Focus</button>
+      <button onClick={handleClear}>Clear</button>
     </div>
   );
 }
---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,21 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+
+const CustomInpunt = forwardRef((props, ref) => {
+    const inputRef = useRef()
+    useImperativeHandle(ref, () => {
+        return {
+            focus: () => {
+                inputRef.current.focus();
+            },
+            clear: () => {
+                inputRef.current.value ='';
+            }
+        }
+    })
+    return (<div>
+        <input placeholder="enter text" {...props} ref={inputRef} />
+    </div>)
+})
+
+export default CustomInpunt;
